0d14c08f977cac73fc1dc2de87b51a2ca8128359
cjlwired <carlos.jose.lazo@gmail.com>
2013-09-25 19:36:50 -0400

    Fix folder nesting in repository.

---
+++ Hello_World_App/Hello_World_AppTests/Hello_World_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  Hello_World_AppTests.m
+//  Hello_World_AppTests
+//
+//  Created by Carlos Lazo on 9/25/13.
+//  Copyright (c) 2013 Carlos Lazo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hello_World_AppTests : XCTestCase
+
+@end
+
+@implementation Hello_World_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
